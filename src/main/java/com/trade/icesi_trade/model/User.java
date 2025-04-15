8f5fc2cd36db09efdd5d7aceedae54c7d1979cc3
Sara Lucia <saludipu71@gmail.com>
2025-04-15 11:59:24 -0500

    feat(security): configured Spring Security components and role-based access control using JWT

--- src/main/java/com/trade/icesi_trade/model/User.java
+++ src/main/java/com/trade/icesi_trade/model/User.java
@@ -1,7 +1,12 @@
 package com.trade.icesi_trade.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,9 +16,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Table(name = "USERS")
@@ -41,7 +43,7 @@ public class User {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<UserRole> userRoles;
 
 }
