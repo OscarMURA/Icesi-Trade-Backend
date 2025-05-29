0b348dd8fcfaa0ac8ae7a41ad7f533cb17cf2961
Oscar Munoz Ramirez <1083870123@u.icesi.edu.co>
2025-05-28 21:34:18 -0500

    fix: Update new DB postgres

--- src/main/java/com/trade/icesi_trade/controller/api/FavoriteProductApiController.java
+++ src/main/java/com/trade/icesi_trade/controller/api/FavoriteProductApiController.java
@@ -1,5 +1,18 @@
 package com.trade.icesi_trade.controller.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.trade.icesi_trade.Service.Interface.FavoriteProductService;
 import com.trade.icesi_trade.dtos.FavoriteProductDto;
 import com.trade.icesi_trade.mappers.FavoriteProductMapper;
@@ -8,12 +21,6 @@ import com.trade.icesi_trade.model.FavoriteProduct;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/favorites")
