8cd76f7e582e6c561d49bd47cb2b505843521f21
Sara Lucia <saludipu71@gmail.com>
2025-06-05 20:27:09 -0500

    mergeeee

--- src/main/java/com/trade/icesi_trade/controller/api/ChatController.java
+++ src/main/java/com/trade/icesi_trade/controller/api/ChatController.java
@@ -1,22 +1,21 @@
 package com.trade.icesi_trade.controller.api;
 
-import com.trade.icesi_trade.Service.Interface.UserService;
-import com.trade.icesi_trade.dtos.UserResponseDto;
-import com.trade.icesi_trade.mappers.UserMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.trade.icesi_trade.Service.Interface.ChatMessageService;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.trade.icesi_trade.Service.Interface.ChatMessageService;
+import com.trade.icesi_trade.Service.Interface.UserService;
+import com.trade.icesi_trade.dtos.UserResponseDto;
+import com.trade.icesi_trade.mappers.UserMapper;
+import com.trade.icesi_trade.model.User;
 
 @RestController
 @RequestMapping("/api/chat")
