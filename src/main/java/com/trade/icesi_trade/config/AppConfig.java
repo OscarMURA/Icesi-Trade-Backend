553ca634cd3df67fb5f7ae27dc82b867eba803f1
Oscar Munoz Ramirez <1083870123@u.icesi.edu.co>
2025-07-03 20:33:46 -0500

    feat: refactoring path

--- src/main/java/com/trade/icesi_trade/config/AppConfig.java
+++ src/main/java/com/trade/icesi_trade/config/AppConfig.java
@@ -42,13 +42,13 @@ public class AppConfig {
     @Order(1)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/Icesi-Trade/api/")
+                .securityMatcher("/api/")
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configure(http))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/Icesi-Trade/api/auth/").permitAll()
-                        .requestMatchers("/Icesi-Trade/ws/").permitAll() // Permite acceso al WebSocket
+                        .requestMatchers("/api/auth/").permitAll()
+                        .requestMatchers("/ws/").permitAll() // Permite acceso al WebSocket
                         .anyRequest().authenticated())
                 .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
@@ -59,26 +59,26 @@ public class AppConfig {
     @Order(2)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/Icesi-Trade/public/", "/Icesi-Trade/css/", "/Icesi-Trade/js/")
+                .securityMatcher("/public/", "/css/", "/js/")
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/Icesi-Trade/public/login",
-                                "/Icesi-Trade/public/register",
-                                "/Icesi-Trade/css/",
-                                "/Icesi-Trade/js/",
-                                "/Icesi-Trade/swagger-ui/",
-                                "/Icesi-Trade/v3/api-docs/",
-                                "/Icesi-Trade/swagger-ui.html")
+                                "/public/login",
+                                "/public/register",
+                                "/css/",
+                                "/js/",
+                                "/swagger-ui/",
+                                "/v3/api-docs/",
+                                "/swagger-ui.html")
                         .permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
-                        .loginPage("/Icesi-Trade/public/login")
-                        .defaultSuccessUrl("/Icesi-Trade/public/default", true)
+                        .loginPage("/public/login")
+                        .defaultSuccessUrl("/public/default", true)
                         .permitAll())
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/Icesi-Trade/public/login?logout")
+                        .logoutSuccessUrl("/public/login?logout")
                         .permitAll())
                 .sessionManagement(sess -> sess
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
